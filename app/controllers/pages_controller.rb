75f3d5c8e7cad4bd496c53095d87c58bf8e85dc0
Francois Faubert <frank@francoisfaubert.com>
2015-01-27 18:29:29 +0000

    removed the before_filter call. It was not very useful. Removed islive validation on twitch model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,27 +2,18 @@ class PagesController < ApplicationController
 
     require 'rails_autolink'
 
-    before_action :before
-
-
-    def before
-        # on each pages we need to know if crank is streaming
-        @twitch = TwitchStream.first
-    end
-
     def index
         @twitch = TwitchStream.first
         @tweets = Tweet.limit(8).order('created_at')
         @youtube = Youtube.order('created_at').first unless @twitch.is_live
-
     end
 
     def stream
-
+        @twitch = TwitchStream.first
     end
 
     def schedule
-
+        @twitch = TwitchStream.first
     end
 
     def videohighlights
